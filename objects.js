1f7aa308c93dc9e1b93c88d8f19786e62273b69c
DGWerlod <ddeanda333@gmail.com>
2017-02-18 23:36:49 -0800

    Add heart calculations

--- objects.js
+++ objects.js
@@ -1,8 +1,8 @@
 var spawnEnemy = function () {
-    enemies.push (new Enemy(1070,Math.random()*(500-50),100,50,"img/axolotl1.png","img/axolotl2.png",10,12));
+    enemies.push (new Enemy(1070,Math.random()*(500-50),100,50,"img/axolotl1.png","img/axolotl2.png",10,1));
 }
 
-var player = new Player(10,250,200,121.2,"img/armadilloWalk1.png","img/armadilloWalk2.png","img/armadilloAttack1.png","img/armadilloAttack2.png",5);
+var player = new Player(10,250,200,121.2,"img/armadilloWalk1.png","img/armadilloWalk2.png","img/armadilloAttack1.png","img/armadilloAttack2.png",3);
 
 var update = function () {
     ctx.clearRect(0,0,1000,500);
@@ -14,16 +14,15 @@ var update = function () {
     }
     player.update();
     
-    ctx.drawImage(hearts[1],10,10,30,30);
-    ctx.drawImage(hearts[1],50,10,30,30);
-    ctx.drawImage(hearts[1],90,10,30,30);
-    ctx.drawImage(hearts[1],130,10,30,30);
-    ctx.drawImage(hearts[1],170,10,30,30);
-    ctx.drawImage(hearts[1],210,10,30,30);
-    ctx.drawImage(hearts[1],250,10,30,30);
-    ctx.drawImage(hearts[1],290,10,30,30);
-    ctx.drawImage(hearts[1],330,10,30,30);
-    ctx.drawImage(hearts[1],370,10,30,30);
+    for (var i = 2; i <= player.hp; i += 2) {
+        ctx.drawImage(hearts[1],(10+(40*(i/2))),10,30,30);
+    }
+    
+    if (player.hp % 2 === 1) {
+        ctx.drawImage(hearts[0],(50+(40*(Math.floor(player.hp/2)))),10,30,30);
+    }
+    
+    var percent = (score%10)/10;
     
     ctx.strokeStyle = "#333333";
     ctx.lineWidth = 5;
@@ -32,7 +31,7 @@ var update = function () {
     ctx.stroke();
     ctx.strokeStyle = "yellow";
     ctx.beginPath();
-    ctx.arc(925,425,50,1.5*Math.PI,2.9*Math.PI);
+    ctx.arc(925,425,50,1.5*Math.PI,((percent*2)+1.5)*Math.PI);
     ctx.stroke();
     
     ctx.textAlign = "center";
